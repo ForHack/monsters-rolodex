5f1fa413daeab0087295ee4cf948ec8623cb5966
ForHack <olimjonov-odiljon@mail.ru>
2020-05-09 14:05:52 +0500

    Add files via upload

---
+++ precache-manifest.8d1aef15ca5017de0f30316f82f6bc40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcf976b777241f5acd4db7fedd4068d1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "104a843456d7b2be742c",
+    "url": "/monsters-rolodex/static/css/main.407dbaec.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "104a843456d7b2be742c",
+    "url": "/monsters-rolodex/static/js/main.524739ed.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
